4753ee401c00ff3e98dee25c01df64d219727e9c
Gurudas Bhardwaj <gursad5@gmail.com>
2026-02-14 12:20:06 +0530

    feat: integrate Mapbox for enhanced mapping functionality
    - Added HomeMap component to display a Mapbox map with geolocation features.
    - Updated Home page to render the HomeMap component.
    - Introduced PrivateLayout component for consistent layout structure.
    - Updated TypeScript configuration to include additional type definitions.
    - Added Mapbox GL and related dependencies to package.json and package-lock.json.
    - Created global.d.ts for Mapbox CSS module declaration.

--- server/src/middleware/tokenVerify.ts
+++ server/src/middleware/tokenVerify.ts
@@ -5,7 +5,7 @@ export const tokenVerify = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Response | void => {
   try {
     const token = req.cookies.refreshToken;
     if (!token) {
@@ -16,7 +16,7 @@ export const tokenVerify = (
       process.env.REFRESH_TOKEN_SECRET!
     ) as JwtPayload;
     req.userId = decoded.userId;
-    next();
+    return next();
   } catch (error) {
     console.log("Error in tokenVerify:", error);
     return res.status(500).json({ errorMsg: "Internal server error", error });
